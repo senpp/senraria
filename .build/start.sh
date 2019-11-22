77dcdd4412a2942fcd181d04bf589198f9a38ebf
pp <phuongthephung@gmail.com>
2019-11-22 11:03:20 +0700

    Add - Environment variable to autocreate or not, select tshock version and some more

--- .build/start.sh
+++ .build/start.sh
@@ -3,4 +3,9 @@
 set -e
 
 [ "$(ls -A /plugins)" ] && cp -f /plugins/* /tshock/ServerPlugins
-exec mono --server --gc=sgen -O=all TerrariaServer.exe -configpath /config -worldpath /world -logpath /logs "$@" -maxplayers 50
+
+if [[ ${AUTOCREATE} = 1 ]] && [[ ! -f /world/${WORLD_NAME}.wld ]]; then
+    exec mono --server --gc=sgen -O=all TerrariaServer.exe -autocreate ${AUTOCREATE_WORLD_SIZE} -world ${WORLD_NAME}.wld -port 7777 -configpath /config -worldpath /world -logpath /logs "$@" -maxplayers 50
+else
+    exec mono --server --gc=sgen -O=all TerrariaServer.exe -configpath /config -worldpath /world -world /world/${WORLD_NAME}.wld -logpath /logs "$@" -maxplayers 50
+fi
--- README.md
+++ README.md
@@ -4,6 +4,8 @@
 
 Based on frolvlad/alpine-mono and forked from [Mark2-0/docker-tshock](https://github.com/Mark2-0/docker-tshock)
 
+![Docker Cloud Build Status](https://img.shields.io/docker/cloud/build/phuongphung/senraria)
+
 ## License
 
 MIT
